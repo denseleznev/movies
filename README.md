d0f9055d3a62a6e6349b492abcd1099007aaff60
Denis Seleznev <dseleznev89@gmail.com>
2016-08-17 00:24:01 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Movie Trailer Website
+A script which generates a static web page allowing to browse a list of my
+favorite movies, including box art imagery and ability to watch a YouTube
+trailer.
+
+## Installation
+Assuming you have installed Git:
+
+`git clone http://github.com/denseleznev/`
+
+## Usage
+Inside the folder `movies` run the following code:
+
+`python2 entertainment_center.py`
+
+The script will then generate HTML page which will automatically open in a
+web browser. Click on any movie poster to watch YouTube trailer.
+
+## License
+This code is licensed under [MIT license](LICENSE.md).
--- entertainment_center.py
+++ entertainment_center.py
@@ -7,22 +7,22 @@ import fresh_tomatoes
 
 # Create our favorite movie objects
 ace_ventura = media.Movie("Ace Ventura: Pet Detective",
-                          "https://upload.wikimedia.org/wikipedia/en/8/84/Ace_ventura_pet_detective.jpg",
+                          "https://upload.wikimedia.org/wikipedia/en/8/84/Ace_ventura_pet_detective.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=QzxDlS6QY1s")
 green_mile = media.Movie("The Green Mile",
-                         "https://upload.wikimedia.org/wikipedia/en/c/ce/Green_mile.jpg",
+                         "https://upload.wikimedia.org/wikipedia/en/c/ce/Green_mile.jpg",  # NOQA
                          "https://www.youtube.com/watch?v=ctRK-4Vt7dA")
 shawshank = media.Movie("The Shawshank Redemption",
-                        "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
+                        "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=6hB3S9bIaco")
 smith = media.Movie("Mr. & Mrs. Smith",
-                    "https://upload.wikimedia.org/wikipedia/en/b/b4/Mr_and_mrs_smith_poster.jpg",
+                    "https://upload.wikimedia.org/wikipedia/en/b/b4/Mr_and_mrs_smith_poster.jpg",  # NOQA
                     "https://www.youtube.com/watch?v=7C1miwFdQOQ")
 pets = media.Movie("The Secret Life of Pets",
-                   "https://upload.wikimedia.org/wikipedia/en/6/64/The_Secret_Life_of_Pets_poster.jpg",
+                   "https://upload.wikimedia.org/wikipedia/en/6/64/The_Secret_Life_of_Pets_poster.jpg",  # NOQA
                    "https://www.youtube.com/watch?v=UZ4WBlveGfw")
 button = media.Movie("The Curious Case of Benjamin Button",
-                     "https://upload.wikimedia.org/wikipedia/en/7/7d/Curious_case_of_benjamin_button_ver3.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/7/7d/Curious_case_of_benjamin_button_ver3.jpg",  # NOQA
                      "https://www.youtube.com/watch?v=VqeqaweXBV0")
 
 # Here we have to make a list of our movie objects because fresh_tomatoes
